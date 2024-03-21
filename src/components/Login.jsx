2f6f6e5c615074520904d7c29064ba8ca33c6868
Rafiulsam <rafiulsam69@gmail.com>
2024-03-22 01:23:06 +0600

    password reset method added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,5 @@
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
-import React, { useState } from 'react';
+import { getAuth, sendPasswordResetEmail, signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useRef, useState } from 'react';
 import app from '../firebase/firebase.init';
 import { Link } from 'react-router-dom';
 
@@ -8,6 +8,7 @@ const auth = getAuth(app)
 const Login = () => {
     const [error, setError] = useState('');
     const [success, setSuccess] = useState('');
+    const emailRef = useRef()
 
     const handleLogin = event => {
         event.preventDefault()
@@ -31,25 +32,40 @@ const Login = () => {
             })
     }
 
+    const handleResetPassword = ()=>{
+        const email = emailRef.current.value;
+        if (!email){
+            alert('Please enter a email address')
+            return
+        }
+        sendPasswordResetEmail(auth, email)
+        .then(()=>{
+            alert('Please check you email')
+        })
+        .catch(error=>{
+            setError(error.message)
+        })
+    }
     return (
         <div className='w-25 mx-auto'>
             <h2>Pease login</h2>
             <form onSubmit={handleLogin}>
                 <div className="mb-3">
                     <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" className="form-control" name='email' id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Your Email' />
+                    <input type="email" ref={emailRef} className="form-control" name='email' id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Your Email' required />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" name='password' className="form-control" id="exampleInputPassword1" placeholder='Your Password' />
+                    <input type="password" name='password' className="form-control" id="exampleInputPassword1" placeholder='Your Password' required/>
                 </div>
-                <div className="mb-3 form-check">
+                <div className="mb-3 d-flex align-items-center form-check">
                     <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                    <label className="form-check-label" htmlFor="exampleCheck1">Remember me</label>
+                    <label className=" ps-1 form-check-label" htmlFor="exampleCheck1">Remember me</label>
+                    <span className='ms-auto'><button onClick={handleResetPassword} className='p-0 btn btn-link'>Forgot password?</button></span>
                 </div>
                 <button type="submit" className="btn btn-primary">Log in</button>
             </form>
-            <p><small>New to this website? Please <Link className='text-decoration-underline' to='/register'>Register</Link></small></p>
+            <p><small>Don't have an account? Please <Link className='text-decoration-underline' to='/register'>Register</Link></small></p>
             <p className='text-danger'>{error}</p>
             <p className='text-success'>{success}</p>
         </div>
